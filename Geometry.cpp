57386c3ea8fa76579e84d7fec38ec459fcc9018c
Ramin Atrian <ratrian@ucsd.edu>
2020-12-02 23:42:06 -0800

    progress for part#1

--- Geometry.cpp
+++ Geometry.cpp
@@ -208,9 +208,16 @@ Geometry::Geometry(std::string objFilename, GLfloat scaleFactor, GLfloat pointSi
 			std::cout << "Cubemap tex failed to load at path: " << textureFilename << std::endl;
 			stbi_image_free(data);
 		}
-		lobbyTexture = textureID;
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 
 		glBindTexture(GL_TEXTURE_2D, 0);
+
+		lobbyTexture = textureID;
 	}
 }
 
